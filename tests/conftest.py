60254a78e30cd872db3ee5334fa4957b052bf7a4
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-20 23:28:58 +0300

    отредактирована фикстура setup и teardown

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,17 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(params=['firefox', 'chrome'])
 def driver(request):
     if request.param == 'firefox':
-        firefox_driver = GeckoDriverManager().install()
-        service = Service(firefox_driver)
-        driver = webdriver.Firefox(service=service)
-    if request.param == 'chrome':
-        chrome_driver = ChromeDriverManager().install()
-        service = Service(chrome_driver)
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+    else:
         options = Options()
         options.add_argument("--window-size=1920,1080")
-        driver = webdriver.Chrome(service=service, options=options)
+        driver = webdriver.Chrome(options=options)
 
     yield driver
     driver.quit()
