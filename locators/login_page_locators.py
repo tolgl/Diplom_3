4eabf7989fdab17c10de4ffeee12c42ac4ab30d9
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-21 22:15:31 +0300

    добавлены новые локаторы

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
     button_password_recovery = (By.XPATH, ".//a[@href='/forgot-password']")
+    link_registration = (By.XPATH, ".//a[@href='/register']")
+    field_email = (By.XPATH, "//input[@name='name']")
+    field_password = (By.XPATH, "//input[@name='Пароль']")
+    button_login = (By.XPATH, ".//button[text() = 'Войти']")
