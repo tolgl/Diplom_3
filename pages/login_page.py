5a55c5121f0579c5f21043de4a928241a395fdc4
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-21 22:16:15 +0300

    добавлены новые методы для авторизации пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-from locators.main_page_locators import MainPageLocators
 from locators.login_page_locators import LoginPageLocators
 from pages.base_page import BasePage
 
@@ -11,6 +10,19 @@ class LoginPageHelper(BasePage):
     def click_on_button_recovery_password(self):
         self.wait_clickable_element(LoginPageLocators.button_password_recovery, wait_time=5).click()
 
-    @allure.step('Получение текста "Восстановить пароль"')
-    def get_h2_password_recovery(self):
-        return self.find_element(LoginPageLocators.h2_password_recovery, wait_time=3).text
+    @allure.step('Нажатие на ccылку "Зарегистрироваться"')
+    def click_link_registration(self):
+        self.wait_clickable_element(LoginPageLocators.link_registration, wait_time=3).click()
+
+    @allure.step('Заполняем поле "Email" на странице авторизации')
+    def filling_field_email_on_login_page(self, email):
+        self.wait_change_url(url_contains='login')
+        self.find_element(LoginPageLocators.field_email, wait_time=5).send_keys(email)
+
+    @allure.step('Заполняем поле "Пароль" на странице авторизации')
+    def filling_field_password_on_login_page(self, password):
+        self.find_element(LoginPageLocators.field_password, wait_time=5).send_keys(password)
+
+    @allure.step('Нажимаем на кнопку "Войти" на странице авторизации')
+    def click_on_button_login(self):
+        self.wait_clickable_element(LoginPageLocators.button_login, wait_time=5).click()
