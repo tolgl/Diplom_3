2e709b181465a49b7699410ef019d9ca2d3bf092
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 09:47:44 +0300

    добавлены два метода

--- pages/personal_account_page.py
+++ pages/personal_account_page.py
@@ -6,6 +6,14 @@ import allure
 
 class PersonalAccountPageHelper(BasePage):
 
-    @allure.step('Получаем текст "История заказа" в ЛК')
+    @allure.step('Получаем текст "История заказов" в ЛК')
     def get_text_link_history_orders(self):
         return self.find_element(PersonalAccountPageLocators.link_history_orders, wait_time=5).text
+
+    @allure.step('Нажимаем на ссылку "История заказов" в ЛК')
+    def click_link_history_orders(self):
+        self.find_element(PersonalAccountPageLocators.link_history_orders, wait_time=5).click()
+
+    @allure.step('Нажимаем на кнопку "Выход" в ЛК')
+    def click_button_logout(self):
+        self.find_element(PersonalAccountPageLocators.button_logout, wait_time=5).click()
