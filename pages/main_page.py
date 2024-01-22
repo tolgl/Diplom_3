c88c10ede89d0945e51c37e24aaefbd9da295070
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 10:43:28 +0300

    добавлены новые методы

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,6 +6,14 @@ import allure
 
 class MainPageHelper(BasePage):
 
-    @allure.step('Нажатие на кнопку "Личный кабинет"')
+    @allure.step('Нажимаем на кнопку "Личный кабинет"')
     def click_on_button_personal_account(self):
         self.wait_clickable_element(MainPageLocators.button_personal_account, wait_time=5).click()
+
+    @allure.step('Нажимаем на ссылку "Конструктор"')
+    def click_on_link_constructor(self):
+        self.wait_clickable_element(MainPageLocators.link_constructor, wait_time=5).click()
+
+    @allure.step('Получаем заголовок главной страницы')
+    def get_text_h1(self):
+        return self.find_element(MainPageLocators.h1, wait_time=5).text
