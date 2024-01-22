fddd155ce6ccae37a4e905b177bcfbdf8813578b
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 11:02:13 +0300

    добавлен локатор ссылки Лента заказов

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -5,3 +5,4 @@ class MainPageLocators:
     button_personal_account = (By.XPATH, ".//a[@href='/account']")
     link_constructor = (By.XPATH, ".//p[text()='Конструктор']//parent::a")
     h1 = (By.XPATH, ".//h1[text()='Соберите бургер']")
+    link_order_feed = (By.XPATH, ".//a[@href='/feed']")
