845164605ea0dee96d6f018417b7230487f7194f
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 11:02:36 +0300

    добавлен метод нажатия на ссылку Лента заказов

--- pages/main_page.py
+++ pages/main_page.py
@@ -17,3 +17,7 @@ class MainPageHelper(BasePage):
     @allure.step('Получаем заголовок главной страницы')
     def get_text_h1(self):
         return self.find_element(MainPageLocators.h1, wait_time=5).text
+
+    @allure.step('Нажимаем на ссылку "Лента заказов"')
+    def click_on_link_order_feed(self):
+        self.wait_clickable_element(MainPageLocators.link_order_feed, wait_time=5).click()
