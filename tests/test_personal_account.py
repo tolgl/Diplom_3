61f583d3a7e0765adc3d5a1c9803c08d397d5639
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 10:41:17 +0300

    заменена фикстура на создание пользователя

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -2,25 +2,18 @@ import allure
 from pages.main_page import MainPageHelper
 from pages.login_page import LoginPageHelper
 from pages.personal_account_page import PersonalAccountPageHelper
-from pages.registration_page import RegistrationPageHelper
 
 
 class TestPersonalAccount:
 
     @allure.title('Проверка перехода на страницу "Личный кабинет"')
-    def test_redirection_on_personal_account_page(self, driver, generation_user_data):
+    def test_redirection_on_personal_account_page(self, driver, creating_new_user):
         main_page = MainPageHelper(driver)
         main_page.go_to_page()
         main_page.click_on_button_personal_account()
         login_page = LoginPageHelper(driver)
-        login_page.click_link_registration()
-        registration_page = RegistrationPageHelper(driver)
-        registration_page.filling_field_name(name=generation_user_data[0])
-        registration_page.filling_field_email(email=generation_user_data[1])
-        registration_page.filling_field_password(password=generation_user_data[2])
-        registration_page.click_on_button_registration()
-        login_page.filling_field_email_on_login_page(email=generation_user_data[1])
-        login_page.filling_field_password_on_login_page(password=generation_user_data[2])
+        login_page.filling_field_email_on_login_page(email=creating_new_user[0])
+        login_page.filling_field_password_on_login_page(password=creating_new_user[1])
         login_page.click_on_button_login()
         main_page.click_on_button_personal_account()
         personal_account_page = PersonalAccountPageHelper(driver)
@@ -30,19 +23,13 @@ class TestPersonalAccount:
         assert actual_result == 'История заказов'
 
     @allure.title('Проверка перехода на страницу "История заказов"')
-    def test_redirection_on_history_orders_page(self, driver, generation_user_data):
+    def test_redirection_on_history_orders_page(self, driver, creating_new_user):
         main_page = MainPageHelper(driver)
         main_page.go_to_page()
         main_page.click_on_button_personal_account()
         login_page = LoginPageHelper(driver)
-        login_page.click_link_registration()
-        registration_page = RegistrationPageHelper(driver)
-        registration_page.filling_field_name(name=generation_user_data[0])
-        registration_page.filling_field_email(email=generation_user_data[1])
-        registration_page.filling_field_password(password=generation_user_data[2])
-        registration_page.click_on_button_registration()
-        login_page.filling_field_email_on_login_page(email=generation_user_data[1])
-        login_page.filling_field_password_on_login_page(password=generation_user_data[2])
+        login_page.filling_field_email_on_login_page(email=creating_new_user[0])
+        login_page.filling_field_password_on_login_page(password=creating_new_user[1])
         login_page.click_on_button_login()
         main_page.click_on_button_personal_account()
         personal_account_page = PersonalAccountPageHelper(driver)
@@ -51,19 +38,13 @@ class TestPersonalAccount:
         assert 'account/order-history' in driver.current_url
 
     @allure.title('Проверка выхода из ЛК')
-    def test_logout_personal_account(self, driver, generation_user_data):
+    def test_logout_personal_account(self, driver, creating_new_user):
         main_page = MainPageHelper(driver)
         main_page.go_to_page()
         main_page.click_on_button_personal_account()
         login_page = LoginPageHelper(driver)
-        login_page.click_link_registration()
-        registration_page = RegistrationPageHelper(driver)
-        registration_page.filling_field_name(name=generation_user_data[0])
-        registration_page.filling_field_email(email=generation_user_data[1])
-        registration_page.filling_field_password(password=generation_user_data[2])
-        registration_page.click_on_button_registration()
-        login_page.filling_field_email_on_login_page(email=generation_user_data[1])
-        login_page.filling_field_password_on_login_page(password=generation_user_data[2])
+        login_page.filling_field_email_on_login_page(email=creating_new_user[0])
+        login_page.filling_field_password_on_login_page(password=creating_new_user[1])
         login_page.click_on_button_login()
         main_page.click_on_button_personal_account()
         personal_account_page = PersonalAccountPageHelper(driver)
