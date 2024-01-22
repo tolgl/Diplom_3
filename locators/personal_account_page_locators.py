cb667c192978ec926ae7b83ff31a7491a6cdc2bd
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 09:48:03 +0300

    добавлен локатор кнопки выхода

--- locators/personal_account_page_locators.py
+++ locators/personal_account_page_locators.py
@@ -3,3 +3,4 @@ from selenium.webdriver.common.by import By
 
 class PersonalAccountPageLocators:
     link_history_orders = (By.XPATH, ".//a[@href='/account/order-history']")
+    button_logout = (By.XPATH, './/button[text()="Выход"]')
