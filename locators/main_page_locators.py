43b70c8e9229533095c2b79cb5e8d8e9a69ca146
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-22 10:43:13 +0300

    добавлены новые локаторы

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -3,3 +3,5 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     button_personal_account = (By.XPATH, ".//a[@href='/account']")
+    link_constructor = (By.XPATH, ".//p[text()='Конструктор']//parent::a")
+    h1 = (By.XPATH, ".//h1[text()='Соберите бургер']")
