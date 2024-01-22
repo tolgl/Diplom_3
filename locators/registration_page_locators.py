8880bdb2291427aac3ed6ca760ddda50686316bd
Anatoly <149782495+tolgl@users.noreply.github.com>
2024-01-22 10:47:27 +0300

    Delete locators/registration_page_locators.py
    регистрация пользователя реализована через фикстуру

--- locators/registration_page_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class RegistrationPageLocators:
-    field_name = (By.XPATH, ".//fieldset[1]//input")
-    field_email = (By.XPATH, ".//fieldset[2]//input")
-    field_password = (By.XPATH, ".//fieldset[3]//input")
-    button_registration = (By.XPATH, ".//button[text() = 'Зарегистрироваться']")
