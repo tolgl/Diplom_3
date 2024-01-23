45d48de9d3b77e5c4e10881b513f3a0e4d33a159
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-23 10:56:07 +0300

    добавлены методы на главной страницы

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from locators.main_page_locators import MainPageLocators
 from pages.base_page import BasePage
+from selenium.webdriver import ActionChains
 
 import allure
 
@@ -21,3 +22,35 @@ class MainPageHelper(BasePage):
     @allure.step('Нажимаем на ссылку "Лента заказов"')
     def click_on_link_order_feed(self):
         self.wait_clickable_element(MainPageLocators.link_order_feed, wait_time=5).click()
+
+    @allure.step('Нажимаем на ингредиент')
+    def click_on_ingredient(self):
+        self.wait_clickable_element(MainPageLocators.first_ingredient_bun, wait_time=5).click()
+
+    @allure.step('Получаем класс модального окна "Детали ингредиента"')
+    def get_class_modal_ingredient(self):
+        return self.find_element(MainPageLocators.modal_ingredient, wait_time=3).get_attribute('class')
+
+    @allure.step('Закрываем модальное окно кликом по крестику')
+    def click_on_button_close_ingredient(self):
+        self.find_element(MainPageLocators.button_close_modal_ingredient, wait_time=5).click()
+
+    @allure.step('Добавляем ингредиент в заказ')
+    def add_ingredient_to_order(self):
+        draggable = self.find_element(MainPageLocators.first_ingredient_bun, wait_time=5)
+        droppable = self.find_element(MainPageLocators.burger_constructor, wait_time=5)
+        ActionChains(self.driver) \
+            .drag_and_drop(draggable, droppable) \
+            .perform()
+
+    @allure.step('Получаем количество ингредиента')
+    def get_count_ingredient(self):
+        return self.find_element(MainPageLocators.ingredient_counter, wait_time=3).text
+
+    @allure.step('Нажимаем на кнопку "Оформить заказ"')
+    def click_on_button_make_order(self):
+        self.find_element(MainPageLocators.button_make_order, wait_time=5).click()
+
+    @allure.step('Получаем класс модального окна оформленного заказа')
+    def get_class_modal_order(self):
+        return self.find_element(MainPageLocators.modal_order, wait_time=3).get_attribute('class')
