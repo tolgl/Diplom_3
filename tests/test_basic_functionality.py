ea60f7301c9a0d3da96e63f12c43bee665c8f4c0
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-25 10:23:41 +0300

    добавлен метод ожидания скрытия лоадера

--- tests/test_basic_functionality.py
+++ tests/test_basic_functionality.py
@@ -85,12 +85,13 @@ class TestBasicFunctionality:
     def test_make_order_authorized_user_without_ingredient(self, driver, creating_new_user):
         main_page = MainPageHelper(driver)
         main_page.go_to_page()
-        time.sleep(1)
+        main_page.wait_hidden_loader()
         main_page.click_on_button_personal_account()
         login_page = LoginPageHelper(driver)
         login_page.filling_field_email_on_login_page(email=creating_new_user[0])
         login_page.filling_field_password_on_login_page(password=creating_new_user[1])
         login_page.click_on_button_login()
+        main_page.wait_hidden_loader()
         main_page.click_on_button_make_order()
         actual_result = main_page.get_class_modal_order()
 
