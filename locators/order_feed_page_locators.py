4eada9c642da3fb82be4ed1cd6c26c2b2d950d31
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-31 11:21:34 +0300

    изменен локатор order_in_work

--- locators/order_feed_page_locators.py
+++ locators/order_feed_page_locators.py
@@ -8,4 +8,4 @@ class OrderFeedPageLocators:
     number_order = (By.XPATH, ".//p[contains(text(), '#')]")
     count_order_for_all_time = (By.XPATH, ".//p[text()='Выполнено за все время:']/parent::div/p[contains(@class, 'OrderFeed_number__2MbrQ')]")
     count_order_for_current_day = (By.XPATH, ".//p[text()='Выполнено за сегодня:']/parent::div/p[contains(@class, 'OrderFeed_number__2MbrQ')]")
-    order_in_work = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady__1YFem')]/li")
+    order_in_work = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady__1YFem')]/li[not(contains(text(), 'Все текущие заказы готовы!'))]")
