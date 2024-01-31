0df1c324613ea171ef0923e4f9fe201ff3547af6
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-31 10:33:06 +0300

    убран порядковый номер в локаторах

--- locators/order_feed_page_locators.py
+++ locators/order_feed_page_locators.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 class OrderFeedPageLocators:
     h1 = (By.XPATH, ".//h1[text()='Лента заказов']")
-    first_order = (By.XPATH, ".//div/ul/li[1]/a")
+    first_order = (By.CLASS_NAME, "OrderHistory_link__1iNby")
     modal_order_details = (By.XPATH, ".//p[text()='Выполнен']/parent::div/parent::div/parent::section")
-    number_order = (By.XPATH, "//ul/li[1]/a/div[1]/p[1]")
-    count_order_for_all_time = (By.XPATH, ".//p[text()='Выполнено за все время:']/parent::div/p[2]")
-    count_order_for_current_day = (By.XPATH, ".//p[text()='Выполнено за сегодня:']/parent::div/p[2]")
-    order_in_work = (By.XPATH, ".//p[text()='В работе:']/parent::div/ul[2]/li")
+    number_order = (By.XPATH, ".//p[contains(text(), '#')]")
+    count_order_for_all_time = (By.XPATH, ".//p[text()='Выполнено за все время:']/parent::div/p[contains(@class, 'OrderFeed_number__2MbrQ')]")
+    count_order_for_current_day = (By.XPATH, ".//p[text()='Выполнено за сегодня:']/parent::div/p[contains(@class, 'OrderFeed_number__2MbrQ')]")
+    order_in_work = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady__1YFem')]/li")
