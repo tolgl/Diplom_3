b10da15031434ff30c41269c6348960e62e10113
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-31 10:36:57 +0300

    убран порядковый номер в локаторe number_order

--- locators/personal_account_page_locators.py
+++ locators/personal_account_page_locators.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.by import By
 class PersonalAccountPageLocators:
     link_history_orders = (By.XPATH, ".//a[@href='/account/order-history']")
     button_logout = (By.XPATH, './/button[text()="Выход"]')
-    number_order = (By.XPATH, ".//ul/li[1]/a/div/p[1]")
+    number_order = (By.XPATH, ".//div[contains(@class, 'OrderHistory_textBox__3lgbs')]/p[contains(@class, 'text_type_digits-default')]")
